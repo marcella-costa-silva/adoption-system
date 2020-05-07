d5cb8d45f36787290e1c686a1100069ead30adf7
Marcella <marcellatsangos@gmail.com>
2020-05-07 17:00:08 -0300

    Initial structure and routes

---
+++ public/js/scripts.js
@@ -0,0 +1,19 @@
+const modalOverlay = document.querySelector('.modal-overlay')
+const cards = document.querySelectorAll('.card')
+const modalClose = document.querySelector('.modal-close')
+const iframe = document.querySelector('iframe')
+
+for (let card of cards) {
+  card.addEventListener('click', () => {
+    const videoId = card.getAttribute('id')
+    window.location.href = `/video?id=${videoId}`
+
+    // modalOverlay.classList.add('active')
+    // iframe.src = `https://www.youtube.com.br/embed/${videoId}`
+  })
+}
+
+// modalClose.addEventListener('click', () => {
+//   modalOverlay.classList.remove('active')
+//   iframe.src = ''
+// })
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const routes = require('./routes')
+
+const server = express()
+
+server.use(express.static('public'))
+server.set('view engine', 'njk')
+
+nunjucks.configure('views', { 
+  express: server, 
+  autoescape: false,
+  noCache: true
+})
+
+server.listen(5000, () => console.log('Server is running'))
