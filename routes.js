914073445175264d386a320a95a718e663998829
Marcella <marcellatsangos@gmail.com>
2020-05-08 09:51:59 -0300

    HTML and CSS structure

--- routes.js
+++ routes.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const routes = express.Router()
 
-routes.get('/', (req, res) => res.send('ok'))
+routes.get('/', (req, res) => res.redirect('/animals/index'))
+routes.get('/animals', (req, res) => res.render('animals/index'))
+routes.get('/people', (req, res) => res.render('people/index'))
 
 module.exports = routes
