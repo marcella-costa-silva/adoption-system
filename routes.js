e0c98eda3740f7ab0e7ed72d2e09f17b96cff117
Marcella <marcellatsangos@gmail.com>
2020-05-08 12:54:42 -0300

    Validating data in the Backend

--- routes.js
+++ routes.js
@@ -7,6 +7,10 @@ routes.get('/animals/create', (req, res) => res.render('animals/create'))
 routes.get('/owners', (req, res) => res.render('owners/index'))
 
 routes.post('/animals', (req, res) => {
+  const keys = Object.keys(req.body) // ["avatar_url","name","birth","gender","specialties"]
+  for (let key of keys) {
+    if (req.body[key] == '') return res.send('Please, fill all fields')
+  }
   return res.send(req.body) // Corpo da requisição POST
 })
 
