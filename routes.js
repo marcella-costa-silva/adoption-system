4ac5df857d8a5f64bbb0958b2c34de04b276196e
Marcella <marcellatsangos@gmail.com>
2020-05-08 13:02:01 -0300

    route functions - create

--- routes.js
+++ routes.js
@@ -1,17 +1,12 @@
 const express = require('express')
 const routes = express.Router()
+const dogs = require('./dogs')
 
 routes.get('/', (req, res) => res.redirect('/animals'))
 routes.get('/animals', (req, res) => res.render('animals/index'))
 routes.get('/animals/create', (req, res) => res.render('animals/create'))
 routes.get('/owners', (req, res) => res.render('owners/index'))
 
-routes.post('/animals', (req, res) => {
-  const keys = Object.keys(req.body) // ["avatar_url","name","birth","gender","specialties"]
-  for (let key of keys) {
-    if (req.body[key] == '') return res.send('Please, fill all fields')
-  }
-  return res.send(req.body) // Corpo da requisição POST
-})
+routes.post('/animals', dogs.post)
 
 module.exports = routes
